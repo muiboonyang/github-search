bb919652519a88b6314ea519875a9f68978c7cd0
muiboonyang <muiboonyang@gmail.com>
2024-05-09 18:38:31 +0800

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -156,9 +156,9 @@ app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 // =======================================
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../../../frontend/dist')))
+app.use(express.static(path.join(__dirname, '/dist')))
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; the beginning slash '/' in the string is important
 app.get('*', (_, res) => {
-    res.sendFile(path.join(__dirname + '/../../../frontend/dist/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/index.html'))
 })
