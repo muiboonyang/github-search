165893ff8650dc0c4a2bcfcba0afa286d4bd9ef7
muiboonyang <muiboonyang@gmail.com>
2024-05-10 13:31:24 +0800

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -157,4 +157,6 @@ app.post("/api/github", async (req: Request, res: Response) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Listening on port: ${port}`));
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
